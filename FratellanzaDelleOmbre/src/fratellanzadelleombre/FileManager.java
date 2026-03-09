be3854e61175a17adc04f2fac1d6f7ee1c18148a
yurinabbs <bianconi.yurinabil@franchettisalviani.net>
2026-03-09 08:49:36 +0100

    Aggiunta progetto NetBeans "FratellanzaDelleOmbre" con classi create e attributi aggiunti per ogni classe

---
+++ FratellanzaDelleOmbre/src/fratellanzadelleombre/FileManager.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fratellanzadelleombre;
+
+/**
+ *
+ * @author bianconi.yurinabil
+ */
+public class FileManager {
+    private GameManager gameManager;
+}
---
+++ FratellanzaDelleOmbre/src/fratellanzadelleombre/GameManager.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fratellanzadelleombre;
+
+/**
+ *
+ * @author bianconi.yurinabil
+ */
+public class GameManager {
+    private Personaggio giocatore;
+    private Nemico nemico;
+}
